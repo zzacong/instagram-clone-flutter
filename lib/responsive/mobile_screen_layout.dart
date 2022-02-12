1951fb6316b482e716085ba54704c0cf2b770b12
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-02-12 20:01:13 +1100

    tutorial: bottom nav bar

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/models/user.dart' as m;
 import 'package:instagram_flutter/providers/user_provider.dart';
 import 'package:instagram_flutter/resources/auth_methods.dart';
+import 'package:instagram_flutter/utils/colors.dart';
 import 'package:provider/provider.dart';
 
 class MobileScreenLayout extends StatefulWidget {
@@ -12,22 +14,90 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  int _page = 0;
+  late PageController _pageController;
+
+  void onNavigationTap(int page) {
+    _pageController.jumpToPage(page);
+  }
+
+  void onPageChanged(int page) {
+    setState(() => _page = page);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     m.User user = Provider.of<UserProvider>(context).user;
     return Scaffold(
-      body: Column(
-        children: [
-          Flexible(child: Container(), flex: 1),
-          Center(child: Text(user.username)),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () => AuthMethods.signOut(),
-            child: const Text('Sign out'),
-          ),
-          Flexible(child: Container(), flex: 1),
-        ],
-      ),
-    );
+        body: PageView(
+          children: [
+            Text('feed'),
+            Text('search'),
+            Text('add post'),
+            Text('favourite'),
+            Text('person'),
+          ],
+          controller: _pageController,
+          onPageChanged: onPageChanged,
+          physics: const NeverScrollableScrollPhysics(),
+        ),
+        bottomNavigationBar: CupertinoTabBar(
+          backgroundColor: mobileBackgroundColor,
+          onTap: onNavigationTap,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: _page == 0 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: _page == 1 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.add_circle,
+                color: _page == 2 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+                color: _page == 3 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: _page == 4 ? primaryColor : secondaryColor,
+              ),
+              label: '',
+              backgroundColor: primaryColor,
+            ),
+          ],
+        ));
   }
 }
