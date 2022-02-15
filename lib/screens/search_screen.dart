7526f660eaf95b64b7575d6512ed4c0f833d1526
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-02-15 23:27:00 +1100

    tutorial: show posts on search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/models/post.dart';
 import 'package:instagram_flutter/models/user.dart';
 import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/utils/constants.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -51,7 +54,6 @@ class _SearchScreenState extends State<SearchScreen> {
                 if (!snapshot.hasData) {
                   return const Center(child: CircularProgressIndicator());
                 }
-
                 return ListView.builder(
                   itemCount: snapshot.data!.size,
                   itemBuilder: (context, index) {
@@ -66,7 +68,46 @@ class _SearchScreenState extends State<SearchScreen> {
                 );
               },
             )
-          : const Text('Posts'),
+          : FutureBuilder(
+              future: FirebaseFirestore.instance.collection('posts').get(),
+              builder: (context,
+                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+
+                var isWeb = MediaQuery.of(context).size.width > webScreenSize;
+                return StaggeredGrid.count(
+                  axisDirection: AxisDirection.down,
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                  children: snapshot.data!.docs.asMap().entries.map(
+                    (e) {
+                      final post = Post.fromSnap(e.value);
+
+                      return StaggeredGridTile.count(
+                        crossAxisCellCount: (e.key % 7 == 0)
+                            ? isWeb
+                                ? 1
+                                : 2
+                            : 1,
+                        mainAxisCellCount: (e.key % 7 == 0)
+                            ? isWeb
+                                ? 1
+                                : 2
+                            : 1,
+                        child: Image.network(
+                          post.postUrl,
+                          fit: BoxFit.cover,
+                          alignment: Alignment.center,
+                        ),
+                      );
+                    },
+                  ).toList(),
+                );
+              },
+            ),
     );
   }
 }
